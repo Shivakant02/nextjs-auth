8f6b2ccedbbc8ffc790feff736952ba7db713182
Shivakant <kants6397@gmail.com>
2024-02-05 23:42:22 +0530

    user schema

---
+++ src/models/usetModel.js
@@ -0,0 +1,12 @@
+import exp from "constants";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    
+
+})
+
+
+const User = mongoose.models.users || mongoose.model("users", userSchema)
+
+export default User;
